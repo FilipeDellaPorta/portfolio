b9fba6b2de84e580ffdd0855db22680b9ba5ea05
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-08-31 19:36:53 -0300

    atualizado e dinamizado titulo portfolio

---
+++ index.js
@@ -0,0 +1,5 @@
+const tituloPortfolio = document.getElementById("titulo-portfolio");
+
+tituloPortfolio.innerHTML = `
+    Olá, sou Filipe Della Porta. Recém-formado em Análise e Desenvolvimento de Sistemas, busco oportunidades de estágio em programação. <strong class="destaque__texto">Tenho afinidade por JavaScript, HTML e CSS.</strong>
+`;
