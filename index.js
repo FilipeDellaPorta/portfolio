d27444bd9d72bf5046ba221e6948e81e7b1f0048
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-11-22 19:43:11 -0300

    movido index.js para pasta js

--- index.js
+++
@@ -1,5 +0,0 @@
-const tituloPortfolio = document.getElementById("titulo-portfolio");
-
-tituloPortfolio.innerHTML = `
-    Olá, sou Filipe Della Porta. Recém-formado em Análise e Desenvolvimento de Sistemas, busco oportunidades de estágio em programação. <strong class="destaque__texto">Tenho afinidade por JavaScript, HTML e CSS.</strong>
-`;
