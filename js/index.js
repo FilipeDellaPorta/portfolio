733e70fe59cc46d118c9b39b7fa01855ae0aaaae
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-25 14:25:51 -0300

    refactor: :recycle: reformado código do portfólio

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const tituloPortfolio = document.getElementById('titulo-portfolio');
-
-tituloPortfolio.innerHTML = `
-    Olá, sou Filipe Della Porta, estudante de Análise e Desenvolvimento de Sistemas, busco oportunidades de estágio em programação. <strong class="destaque__texto">Tenho afinidade por Java, TypeScript/JavaScript, HTML e CSS.</strong>
-`;
